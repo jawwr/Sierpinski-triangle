3e650d26cda86a8e71fc0efc4823737e7f4ea39f
Arseny <20mineslend14@gmail.com>
2022-09-29 20:33:32 +0500

    небольшой рефактор, добавлена возможность задавать начальные точки

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -28,57 +28,41 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
-
-  final Duration _duration = const Duration(microseconds: 10);
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final Duration _duration = const Duration(microseconds: 1);
   late Timer _counterTimer;
 
-  final List<Point> _widgets = [
-    Point(
-      vertical: 10,
-      horizontal: 700,
-    ),
-    Point(
-      vertical: 600,
-      horizontal: 250,
-    ),
-    Point(
-      vertical: 600,
-      horizontal: 1200,
-    ),
-  ];
-  int count =  100000;
+  //начальные точки главного треугольника
+  final List<Point> _points = [];
+
+  bool _isAutoPaintingFractal = false;
 
   @override
   void initState() {
     super.initState();
     _counterTimer = Timer.periodic(_duration, (timer) {
-      if(_widgets.length <= count){
-        _fractal();
-      }else{
-        _counterTimer.cancel();
+      if (_isAutoPaintingFractal) {
+        _calculateFractal();
       }
     });
   }
 
-  void _fractal() {
-    // for (int i = 0; i < 10000; i++) {
-      var randVertex = (Random().nextDouble() * 3).floor();
-      var randPoint = (Random().nextDouble() * _widgets.length).floor();
+  void _calculateFractal() {
+    var randVertex = (Random().nextDouble() * 3).floor();
+    var randPoint = (Random().nextDouble() * _points.length).floor();
 
-      var x = _widgets[randVertex].horizontal +
-          (_widgets[randPoint].horizontal - _widgets[randVertex].horizontal) /
-              2;
-      var y = _widgets[randVertex].vertical +
-          (_widgets[randPoint].vertical - _widgets[randVertex].vertical) / 2;
-      var point = Point(
-        horizontal: x,
-        vertical: y,
-      );
-      setState(() {
-        _widgets.add(point);
-      });
-    // }
+    var x = _points[randVertex].horizontal +
+        (_points[randPoint].horizontal - _points[randVertex].horizontal) / 2;
+    var y = _points[randVertex].vertical +
+        (_points[randPoint].vertical - _points[randVertex].vertical) / 2;
+    var point = Point(
+      horizontal: x,
+      vertical: y,
+    );
+    setState(() {
+      _points.add(point);
+    });
   }
 
   @override
@@ -93,18 +77,47 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       ),
       child: Padding(
         padding: const EdgeInsets.all(0),
-        child: Stack(
-          children: _widgets,
-        ),
+        child: _isAutoPaintingFractal
+            ? Stack(
+                children: _points,
+              )
+            : GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                child: Stack(
+                  children: _points,
+                ),
+                onTapDown: (tap) {
+                  double x = tap.localPosition.dx.floorToDouble();
+                  double y = tap.localPosition.dy.floorToDouble();
+
+                  setState(() {
+                    _points.add(Point(
+                      vertical: y,
+                      horizontal: x,
+                      size: 3,
+                    ));
+                  });
+
+                  if (_points.length == 3) {
+                    _isAutoPaintingFractal = true;
+                  }
+                },
+              ),
       ),
     );
   }
 }
 
 class Point extends StatelessWidget {
-  Point({Key? key, this.vertical = 0, this.horizontal = 0}) : super(key: key);
+  const Point(
+      {Key? key,
+      required this.vertical,
+      required this.horizontal,
+      this.size = 1.5})
+      : super(key: key);
   final double vertical;
   final double horizontal;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
@@ -112,10 +125,10 @@ class Point extends StatelessWidget {
       left: horizontal,
       top: vertical,
       child: Container(
-        width: 2,
-        height: 2,
+        width: size,
+        height: size,
         decoration: BoxDecoration(
-          color: Colors.red,
+          color: Colors.deepPurpleAccent,
           borderRadius: BorderRadius.circular(50),
         ),
       ),
