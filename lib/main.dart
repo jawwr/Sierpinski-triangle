968ac64c1c8ebba867e62468d1f50c1e3088ddcb
Arseny <20mineslend14@gmail.com>
2022-10-05 21:25:19 +0500

    переделан на рекурсию

--- lib/main.dart
+++ lib/main.dart
@@ -28,41 +28,67 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  final Duration _duration = const Duration(microseconds: 1);
-  late Timer _counterTimer;
-
+class _MyHomePageState extends State<MyHomePage> {
   //начальные точки главного треугольника
   final List<Point> _points = [];
 
   bool _isAutoPaintingFractal = false;
+  List<Function> func = [];
 
-  @override
-  void initState() {
-    super.initState();
-    _counterTimer = Timer.periodic(_duration, (timer) {
-      if (_isAutoPaintingFractal) {
-        _calculateFractal();
+  Future<void> _start() async {
+    Future.delayed(const Duration(seconds: 1), () {
+      _isAutoPaintingFractal = true;
+      var points = List.of(_points);
+      _serpinskii(points, 8);
+    });
+
+    Future.delayed(const Duration(seconds: 1), () async {
+      for (int i = 0; i < func.length; i++) {
+        await Future.delayed(const Duration(microseconds: 50), () {
+          func[i]();
+        });
       }
     });
   }
 
-  void _calculateFractal() {
-    var randVertex = (Random().nextDouble() * 3).floor();
-    var randPoint = (Random().nextDouble() * _points.length).floor();
+  @override
+  void initState() {
+    super.initState();
+  }
 
-    var x = _points[randVertex].horizontal +
-        (_points[randPoint].horizontal - _points[randVertex].horizontal) / 2;
-    var y = _points[randVertex].vertical +
-        (_points[randPoint].vertical - _points[randVertex].vertical) / 2;
-    var point = Point(
-      horizontal: x,
-      vertical: y,
-    );
-    setState(() {
-      _points.add(point);
-    });
+  Point getMid(Point p1, Point p2) {
+    return Point(
+        vertical: (p1.vertical + p2.vertical) / 2,
+        horizontal: (p1.horizontal + p2.horizontal) / 2);
+  }
+
+  void _draw(List<Point> points) {
+    for (Point p in points) {
+      setState(() {
+        _points.add(p);
+      });
+    }
+  }
+
+  void _serpinskii(List<Point> points, int degree) {
+    func.add(() => _draw(points));
+    if (degree > 0) {
+      _serpinskii([
+        points[0],
+        getMid(points[0], points[1]),
+        getMid(points[0], points[2]),
+      ], degree - 1);
+      _serpinskii([
+        points[1],
+        getMid(points[0], points[1]),
+        getMid(points[1], points[2]),
+      ], degree - 1);
+      _serpinskii([
+        points[2],
+        getMid(points[2], points[1]),
+        getMid(points[0], points[2]),
+      ], degree - 1);
+    }
   }
 
   @override
@@ -97,14 +123,30 @@ class _MyHomePageState extends State<MyHomePage>
                       size: 3,
                     ));
                   });
-
                   if (_points.length == 3) {
-                    _isAutoPaintingFractal = true;
+                    _start();
                   }
                 },
               ),
       ),
     );
+  }
+
+  void _calculateFractal() {
+    var randVertex = (Random().nextDouble() * 3).floor();
+    var randPoint = (Random().nextDouble() * _points.length).floor();
+
+    var x = _points[randVertex].horizontal +
+        (_points[randPoint].horizontal - _points[randVertex].horizontal) / 2;
+    var y = _points[randVertex].vertical +
+        (_points[randPoint].vertical - _points[randVertex].vertical) / 2;
+    var point = Point(
+      horizontal: x,
+      vertical: y,
+    );
+    setState(() {
+      _points.add(point);
+    });
   }
 }
 
